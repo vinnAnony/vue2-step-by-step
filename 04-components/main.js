ba381da6060528a809227409ce17661c1fea5064
Vmbugua <vmbugua@cytonn.com>
2022-01-23 15:10:15 +0300

    Notification card component

--- 04-components/main.js
+++ 04-components/main.js
@@ -2,6 +2,33 @@ Vue.component('student', {
     template: '<li><slot></slot></li>'
 });
 
+Vue.component('card', {
+    props: ['title', 'body'],
+    template: `
+   <div class="min-w-0 p-4 text-white bg-purple-600 rounded-lg shadow-xs mx-4 my-4" v-show="isVisible">
+       <div class="overflow-auto">
+           <h4 class="mb-4 font-semibold float-left">
+               {{title}}
+           </h4>
+           <div class="float-right cursor-pointer" @click="hideModal">x</div>
+       </div>
+        <p>
+          {{body}}
+        </p>
+      </div>
+    `,
+    data() {
+        return {
+            isVisible : true
+        }
+    },
+    methods: {
+        hideModal: function() {
+            this.isVisible = false;
+        }
+    }
+});
+
 Vue.component('student-list', {
     template: `
         <div>
